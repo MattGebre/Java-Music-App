9c7d0df6b456b8f5ba02bb6fc13b1b9eddc1b604
MattGebre <115193209+MattGebre@users.noreply.github.com>
2024-03-25 21:58:17 -0400

    Delete Java Music App directory

--- Java Music App/AlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-public class AlreadyExistsException extends RunTimeException
-	{
-		public AlreadyExistsException() {}
-		public AlreadyExistsException(String message)
-		{
-			super(message);
-		}
-	}
